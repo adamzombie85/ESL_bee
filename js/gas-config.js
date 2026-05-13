74277118f34a426a9035fb9c30bf0f230c5439a1
adamzombie85 <adamzombie85@gmail.com>
2026-05-13 17:37:30 +0800

    Update GAS Web App URL for Google Sheets sync

--- js/gas-config.js
+++ js/gas-config.js
@@ -1,3 +1,3 @@
 // gas-config.js
 // 請在部署 Google Apps Script 後，將「網頁應用程式網址」貼在下方
-const GAS_WEB_APP_URL = "YOUR_GAS_WEB_APP_URL_HERE";
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwbEjoc-QAvzDbGBt5ua_GMwRnTEYpl8afqDcfOkN5MpkCx7k9X0tmvYxhZfheawrJa/exec";
